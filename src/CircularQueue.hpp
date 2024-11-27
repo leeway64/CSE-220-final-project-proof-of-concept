ede0dd2e1d2979855b12f7c6ea63b5e3e02f1a19
Lee-Way Wang <lee.way56@comcast.net>
2024-11-27 02:06:50 -0800

    Add begin() and end() functions, add more unit tests

--- src/CircularQueue.hpp
+++ src/CircularQueue.hpp
@@ -5,80 +5,92 @@ class CircularQueue
 {
     private:
         std::vector<T> circularQueue{};
-        int size = 0;
+        int max_size = 0;
         int counter = 0;
         int back_index = 0;
         int front_index = 0;
+        void unroll_queue();
+        std::vector<T> unrolled_queue{};
 
-    
     public:
+
         // Initializes a vector with size equal to size
-        CircularQueue(int size);
+        CircularQueue(int max_size);
 
         CircularQueue(std::vector<T> list);
     
         std::vector<T> get_queue();
 
         int get_size();
+        
+        int get_max_size();
 
         void push(T value);
 
-        int pop();
+        T pop();
 
         T get_front();
 
         T get_back();
+
+        auto begin();
+
+        auto end();
 };
 
 template<class T>
-CircularQueue<T>::CircularQueue(int size)
+CircularQueue<T>::CircularQueue(int max_size)
 {
-    this->size = size;
-    this->circularQueue.reserve(size);
+    this->max_size = max_size;
+    this->circularQueue.reserve(max_size);
     this->back_index = 0;
-    this->front_index = (this->counter - 1) % size;
+    this->front_index = (this->counter - 1) % max_size;
 }
 
 template <class T>
 CircularQueue<T>::CircularQueue(std::vector<T> list)
 {
     this->circularQueue = list;
-    this->size = list.size();
-    this->circularQueue.reserve(this->size);
-    this->counter = this->size;
-    this->front_index = (this->counter - 1) % size;
+    this->max_size = list.size();
+    this->circularQueue.reserve(this->max_size);
+    this->counter = this->max_size;
+    this->front_index = (this->counter - 1) % max_size;
     this->back_index = 0;
 }
 
 template<class T>
 void CircularQueue<T>::push(T value)
 {
-    if (this->counter < this->size)
+    if (this->counter < this->max_size)
     {
         this->circularQueue.push_back(value);
     }
     else
     {
-        int index = counter % size;
+        int index = counter % this->max_size;
         auto position = this->circularQueue.begin() + index;
         this->circularQueue.erase(position);
         this->circularQueue.insert(position, value);
 
-        this->back_index = (counter + 1) % size;
+        this->back_index = (counter + 1) % max_size;
     }
 
     this->counter++;
-    this->front_index = (this->counter - 1) % size;
+    this->front_index = (this->counter - 1) % max_size;
 }
 
 template<class T>
-int CircularQueue<T>::pop()
+T CircularQueue<T>::pop()
 {
-    int value = this->circularQueue[back_index];
+    T value = this->circularQueue[back_index];
     auto position = this->circularQueue.begin() + back_index;
 
     this->circularQueue.erase(position);
     
+    if (this->circularQueue.size() == 1)
+    {
+        this->front_index = 0;
+    }
     if (this->back_index > this->circularQueue.size() - 1)
     {
         this->back_index = 0;
@@ -96,7 +108,13 @@ std::vector<T> CircularQueue<T>::get_queue()
 template<class T>
 int CircularQueue<T>::get_size()
 {
-    return this->size;
+    return this->circularQueue.size();
+}
+
+template<class T>
+int CircularQueue<T>::get_max_size()
+{
+    return this->max_size;
 }
 
 template<class T>
@@ -109,4 +127,36 @@ template<class T>
 T CircularQueue<T>::get_back()
 {
     return this->circularQueue[back_index];
+}
+
+template<class T>
+void CircularQueue<T>::unroll_queue()
+{
+    unrolled_queue = {};
+    int i = this->front_index;
+    while (i != back_index)
+    {
+        this->unrolled_queue.push_back(this->circularQueue[i]);
+        --i;
+
+        if (i < 0)
+        {
+            i = this->circularQueue.size() - 1;
+        }
+    }
+    this->unrolled_queue.push_back(this->circularQueue[i]);
+}
+
+template<class T>
+auto CircularQueue<T>::begin()
+{
+    CircularQueue<T>::unroll_queue();
+    return this->unrolled_queue.begin();
+}
+
+template<class T>
+auto CircularQueue<T>::end()
+{
+    CircularQueue<T>::unroll_queue();
+    return this->unrolled_queue.end();
 }
--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -13,10 +13,12 @@ TEST_CASE("CircularQueue", "[CircularQueue]")
     CircularQueue<int> cq2 = CircularQueue<int>(vec1);
     CircularQueue<int> cq3 = CircularQueue<int>(size2);
     CircularQueue<int> cq4 = CircularQueue<int>(vec2);
+    CircularQueue<std::string> cq5(size1);
+    CircularQueue<double> cq6(size2);
 
     SECTION("Constructors")
     {
-        REQUIRE(cq1.get_size() == size1);
+        REQUIRE(cq1.get_size() == 0);
         REQUIRE(cq1.get_queue() == std::vector<int>{});
 
         REQUIRE(cq2.get_size() == vec1.size());
@@ -82,6 +84,7 @@ TEST_CASE("CircularQueue", "[CircularQueue]")
             REQUIRE(cq3.get_front() == 9);
             REQUIRE(cq3.get_back() == 8);
             REQUIRE(cq3.get_queue() == std::vector<int>{9, 8});
+            REQUIRE(cq3.get_size() == 2);
 
             REQUIRE(cq3.pop() == 8);
             REQUIRE(cq3.get_front() == 9);
@@ -90,7 +93,7 @@ TEST_CASE("CircularQueue", "[CircularQueue]")
 
             REQUIRE(cq3.pop() == 9);            
             REQUIRE(cq3.get_queue() == std::vector<int>{});
-            REQUIRE(cq3.get_queue().size() == 0);
+            REQUIRE(cq3.get_size() == 0);
         }
 
         SECTION("Constructor that accepts vector")
@@ -104,6 +107,63 @@ TEST_CASE("CircularQueue", "[CircularQueue]")
             REQUIRE(cq4.pop() == 34546);
             REQUIRE(cq4.get_back() == 435435);
             REQUIRE(cq4.get_queue() == std::vector<int>{435435, 135, 54654, 1, 4, 90, 6, 7, 8});
+        }
+    }
+
+    SECTION("Different data types")
+    {
+        cq5.push("Hello");
+        cq5.push("world");
+        REQUIRE(cq5.get_front() == "world");
+        REQUIRE(cq5.get_back() == "Hello");
+        REQUIRE(cq5.get_size() == 2);
+
+        REQUIRE(cq5.pop() == "Hello");
+        REQUIRE(cq5.get_front() == "world");
+        REQUIRE(cq5.get_back() == "world");
+        REQUIRE(cq5.get_size() == 1);
+
+
+        REQUIRE(cq5.pop() == "world");
+        REQUIRE(cq5.get_queue() == std::vector<std::string>{});
+        REQUIRE(cq5.get_size() == 0);
+    }
+
+    SECTION("For-each loop")
+    {
+        const std::vector<int> test_vector1 = {5, 4, 3, 2, 1};
+        const std::vector<double> test_vector2 = {0.32323201, 1893.234324, 12332.3323};
+        SECTION("With integers")
+        {
+            REQUIRE(*cq2.begin() == 5);
+            REQUIRE(*(cq2.end() - 1) == 1);
+
+            auto iter = cq2.begin();
+            int i = 0;
+            while(iter != cq2.end())
+            {
+                REQUIRE(*iter == test_vector1[i]);
+                iter++;
+                i++;
+            }
+        }
+
+        SECTION("With doubles")
+        {
+            cq6.push(12.3);
+            cq6.push(12332.3323);
+            cq6.push(1893.234324);
+            cq6.push(0.32323201);
+            REQUIRE(*cq6.begin() == 0.32323201);
+            REQUIRE(*(cq6.end() - 1) == 12332.3323);
+
+            int i = 0;
+            for (auto iter = cq6.begin(); iter != cq6.end();)
+            {
+                REQUIRE(*iter == test_vector2[i]);
+                ++iter;
+                ++i;
+            }
         }
     }
 }
